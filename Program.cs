11bbc4f2803cce3fbd19f59931b90d55f4b56731
Cole Shepherd <cole@recroom.com>
2019-11-26 19:54:45 -0800

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompanyNameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
